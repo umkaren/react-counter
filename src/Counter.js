7767ef8340a89397a786ab4ad9decc82b740bc2f
Karen Ngo <uminhngo@gmail.com>
2023-04-26 18:30:42 -0400

    created react counter app

---
+++ src/Counter.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+    return (
+        <div>
+            <p>Count: {count}</p>
+        <button onClick={decrement}>Minus 1</button>;
+        <button onClick={increment}>Add 1</button>;
+        </div>
+    )
+  }
